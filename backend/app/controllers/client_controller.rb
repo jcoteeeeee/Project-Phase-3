659bf68e8d1642f49b89c80811b637efb15b2adf
John Cote <johncote61598@gmail.com>
2023-01-04 11:26:15 -0500

    filled out models, migrations

--- backend/app/controllers/client_controller.rb
+++ backend/app/controllers/client_controller.rb
@@ -1,2 +1,5 @@
 class ClientController < ApplicationController
+    has_many :appointments
+    has_many :tattoos, through: :appointments
+    belongs_to :artist
 end
--- backend/app/models/appointment.rb
+++ backend/app/models/appointment.rb
@@ -1,2 +1,5 @@
 class Appointment < ApplicationRecord
+    belongs_to :clients
+    belongs_to :tattoo
+    belongs_to :artist
 end
