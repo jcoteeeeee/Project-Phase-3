94913a525fa12def14efcde2a944cc225a22a58b
John Cote <johncote61598@gmail.com>
2023-01-04 17:53:49 -0500

    added new AptCalendar component

---
+++ frontend/src/components/AptCalendar.jsx
@@ -0,0 +1,12 @@
+import DayTimePicker from '@mooncake-dev/react-day-time-picker'
+
+const AptCalendar = () => {
+    return (
+        <div>
+            <DayTimePicker />
+        </div>
+    )
+
+}
+
+export default AptCalendar
