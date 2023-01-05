3237c10add0c81598a05515eba7e94c5bdf801f9
John Cote <johncote61598@gmail.com>
2023-01-05 16:27:26 -0500

    added info to staffpage and bookinglistpage

--- frontend/src/pages/BookingListPage.jsx
+++ frontend/src/pages/BookingListPage.jsx
@@ -1,10 +1,31 @@
-import { React, useState, } from "react";
+import { React, useEffect, useState, } from "react";
 import { Link } from "react-router-dom";
 
 const BookingListPage = () => {
+
+  const [apts, setApts] = useState([])
+
+  useEffect(() => {
+    const request = async () => {
+      let req = await fetch("localhost//:3000")
+      let res = await req.json()
+      setApts(res)
+    }
+    request()
+  }, [])
+
   return (
     <div>
       <h3>This is a list of all the appts</h3>
+      <div>
+        {
+          res.map((apt) => {
+            return(
+              <div>apt.{apt_datetime}</div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 
--- frontend/src/pages/StaffPage.jsx
+++ frontend/src/pages/StaffPage.jsx
@@ -1,9 +1,33 @@
-import { React, useState, } from "react";
+import { React, useEffect, useState, } from "react";
 
 const StaffPage = () => {
+
+  const [artists, setArtists] = useState([])
+
+  useEffect(() => {
+    const request = async () => {
+      let req = await fetch("localhost//:3000")
+      let res = await req.json()
+      setArtists(res)
+    }
+    request()
+  }, [])
+
   return (
     <div>
       <h3>This is a list of all the artists at this studio</h3>
+      <div>
+        {
+          res.map((artist) => {
+            return(
+              <div>
+                <div>artist.{first_name} + artist.{last_name}</div>
+                <div>artist.{email}</div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
